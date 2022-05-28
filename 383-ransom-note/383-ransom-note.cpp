24868ef0351c22c72a7ce748531469f8c24c027f
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-05-29 01:11:10 +0530

    Time: 40 ms (16.56%), Space: 8.9 MB (14.62%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        map<char,int>m1;
+        map<char,int>m2;
+        for(auto it:magazine)
+        {
+            m2[it]+=1;
+        }
+        for(auto it:ransomNote)
+        {
+            m2[it]--;
+            if(m2[it]<0)
+                return false;
+        }
+        return true;       
+        
+    }
+};
