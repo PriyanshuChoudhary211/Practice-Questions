c6a62f0f61ad7e302e88a08ab1c4c842f3edc23a
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-06-01 22:22:37 +0530

    Time: 11 ms (75.69%), Space: 10.8 MB (27.04%) - LeetHub

--- 6-zigzag-conversion/6-zigzag-conversion.cpp
+++ 6-zigzag-conversion/6-zigzag-conversion.cpp
@@ -1,52 +1,21 @@
 class Solution {
 public:
-    string convert(string s, int r) {
-        char arr[r][1000];
-        string ans="";
-        memset(arr,'#',sizeof(arr));
-        int n = s.length();
-        int i = 0;
-        int k = 0,flag=0;
-        int j = 0;
-        while(k<n)
-        {
-            i=0;
-            while(i<r)
-            {
-                arr[i][j] = s[k++];
-                if(k>=s.length())
-                {
-                        flag=1;
-                    break;
-                }
-                
-                i++;
-            }
-            if(flag==1)
-                break;
-            i = r-2;
-            j++;
-            while(i>0)
-            {
-                arr[i][j] = s[k++];
-                  if(k>=s.length())
-                {
-                        flag=1;
-                    break;
-                }
-                
-                i--;
-                j++;
-            }
-            if(flag==1)
-                break;
+    string convert(string s, int numRows) {
+        if (numRows == 1) return s;
+        
+        vector<string> v(min(numRows, int(s.size())), "");
+        // direction -1 is down, 1 is up
+        int direction = -1, idx = 0;
+        
+        for (int i = 0; i < s.size(); i++) {
+            v[idx] += s[i];
+            idx += direction == -1 ? 1 : -1;
+            if (idx == 0 || idx == numRows - 1) direction = -direction;
         }
-        for(int i = 0; i<r;i++)
-        {
-            for(int m = 0; m<=j;m++)
-                if(arr[i][m]!='#')
-                  ans+=arr[i][m];
-        }
-        return ans;
-        }
+        
+        string res = "";
+        for (auto a : v) res += a;
+        
+        return res;
+    }
 };
