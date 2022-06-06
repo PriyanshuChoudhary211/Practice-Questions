ad5f92975fdad2a7591903ca0be37ada78bc7b7f
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-06-07 00:11:02 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.28%) - LeetHub

---
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumSum(int num){
+        string s = to_string(num);
+        sort(s.begin(), s.end());
+        int x = (s[0] - '0' ) * 10 + s[2] - '0' ;
+        int y = (s[1] - '0' ) * 10 + s[3] - '0' ;
+
+        return x+y;
+    }
+};
