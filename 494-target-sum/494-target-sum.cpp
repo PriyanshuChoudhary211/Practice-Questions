f3ae856f07c0d3f827707a0d2d063fe402b47a9b
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-06-14 23:32:10 +0530

    Time: 23 ms (60.11%), Space: 9.2 MB (55.96%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[21][2001];
+
+    int f(int index, vector<int>& nums, int &S, int sum){
+        if(index==nums.size())
+            return sum==S?1:0;
+        if(dp[index][sum+1000]!=-1) return dp[index][sum+1000];
+        
+        int count = 0;        
+        count+= f(index+1,nums,S,sum+nums[index]);
+        count+= f(index+1,nums,S,sum-nums[index]);
+        
+        return dp[index][sum+1000] = count;
+    }
+    
+    int findTargetSumWays(vector<int>& nums, int S) {
+        memset(dp,-1,sizeof(dp));
+        return f(0,nums,S,0);
+    }
+};
