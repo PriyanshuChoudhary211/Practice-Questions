f3f7340590cd235c45ef7c2d4c77efc788e03616
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-06-18 01:50:57 +0530

    Time: 15 ms (59.82%), Space: 13.7 MB (8.90%) - LeetHub

---
+++ 1286-iterator-for-combination/1286-iterator-for-combination.cpp
@@ -0,0 +1,28 @@
+class CombinationIterator {
+public:
+    vector<string> arr;
+    int pos = 0;
+    
+    void solve(string &s, int len, int pos, string op){
+        if(pos==s.size()){
+            if(op.size()==len) arr.push_back(op);
+            return;
+        }
+        solve(s,len,pos+1,op+s[pos]);
+        solve(s,len,pos+1,op);
+    }
+    
+    CombinationIterator(string characters, int combinationLength) {
+        string op = "";
+        solve(characters, combinationLength, 0, op);
+    }
+    
+    string next() {
+        string ans = arr[pos++];
+        return ans;
+    }
+    
+    bool hasNext() {
+        return pos<arr.size();
+    }
+};
