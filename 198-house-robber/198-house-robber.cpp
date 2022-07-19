16cc47c4edea0d2e7aabce79b806016d82930b30
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-07-19 12:43:30 +0530

    Time: 2 ms (57.69%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,41 +1,17 @@
 class Solution {
 public:
-    // int func(vector<int>&nums,int ind,vector<int>&dp)
-    // {
-    //     if(ind==0) return nums[ind];
-    //     if(ind<0) return 0;
-    //     if(dp[ind]!=-1)return dp[ind];
-    //     int pick = nums[ind] + func(nums,ind-2,dp);
-    //     int notPick = 0 + func(nums,ind-1,dp);
-    //     dp[ind] = max(pick,notPick);
-    //     return dp[ind];        
-    // }
+    int robHouse(vector<int>&nums,vector<int>&dp,int n)
+    {
+        if(n==nums.size()-1)
+            return nums[n];
+        else if(n>nums.size()-1) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int np = robHouse(nums,dp,n+1);
+        int pick = nums[n] + robHouse(nums,dp,n+2);
+        return dp[n]=max(np,pick);        
+    }
     int rob(vector<int>& nums) {
-        //USING MEMORY
-        
-        // vector<int>dp(nums.size());
-        // dp[0] = nums[0];
-        // for(int i = 1; i<nums.size();i++)
-        // {
-        //     int take = nums[i];
-        //     if(i>1) take+=dp[i-2];
-        //     int nottake = 0 + dp[i-1];
-        //     dp[i] = max(take,nottake);
-        // }
-        // return dp[dp.size()-1];     
-        
-        //WITHOUT SPACE
-        
-        int prev=nums[0],prev2=0,curr=0;
-        for(int i = 1; i<nums.size();i++)
-        {
-            int take = nums[i]+prev2;
-            int nottake = prev;
-            curr = max(take,nottake);
-            prev2 =prev;
-            prev = curr;
-        }
-        return prev;
-        
+        vector<int>dp(nums.size()+1,-1);
+        return robHouse(nums,dp,0);
     }
 };
