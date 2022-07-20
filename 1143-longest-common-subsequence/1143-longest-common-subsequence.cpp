8d8692969d460eb370b72a5506f0e8a1d32cdee7
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-07-20 11:46:40 +0530

    Time: 56 ms (18.51%), Space: 13.3 MB (10.98%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,7 +4,7 @@ public:
     {
         if(n1<0||n2<0)
             return 0;
-        if(dp[n1][n2]!=-1) return dp[n1][n2];
+        if(dp[n1+1][n2+1]!=-1) return dp[n1+1][n2+1];
         int x=0,y=0,c=0;
         if(s1[n1]==s2[n2])  c = 1+func(s1,s2,n1-1,n2-1,dp);
         else
@@ -12,11 +12,36 @@ public:
             x = func(s1,s2,n1-1,n2,dp);
             y=func(s1,s2,n1,n2-1,dp);
         }
-        return dp[n1][n2]=c+max(x,y);
+        return dp[n1+1][n2+1]=c+max(x,y);
     }
     int longestCommonSubsequence(string text1, string text2) {
         int n1=text1.length()-1,n2=text2.length()-1;
-         vector<vector<int>>dp(n1+1,vector<int>(n2+1,-1));
-        return func(text1,text2,n1,n2,dp);
+         vector<vector<int>>dp(n1+2,vector<int>(n2+2,-1));
+        int x= func(text1,text2,n1,n2,dp);
+        string s="";
+        int i=n1+1,j=n2+1;
+        //FOR PRINTING THE LONGEST COMMON SUBSEQUENCE 
+        // for(int i = 0;i<=n1+1;i++)
+        // {
+        //     for(int j=0;j<=n2+1;j++)
+        //         cout<<dp[i][j]<<" ";
+        //     cout<<endl;
+        // }
+        // while(i>0 && j>0)
+        // {
+        //     if(text1[i-1]==text2[j-1])
+        //     {
+        //         s = text1[i-1]+s;
+        //         i--;
+        //         j--;
+        //     }
+        //     else if(dp[i][j-1]>dp[i-1][j])
+        //     {
+        //         j=j-1;
+        //     }
+        //     else i=i-1;
+        // }
+        // cout<<s;
+          return x;
     }
 };
