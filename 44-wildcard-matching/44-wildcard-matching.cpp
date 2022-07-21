23953d30e13c990ec3d96f20e133e84f621f59f7
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-07-21 13:18:48 +0530

    Time: 114 ms (36.69%), Space: 27.9 MB (18.29%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool func(string &s, string &t , int i,int j, vector<vector<int>>&dp)
+    {
+        if(i<0 && j<0) return 1;
+        else if(j<0)return 0;
+        else if(i<0)
+        {
+            for(int ii = j;ii>=0;ii--)
+            {
+                if(t[ii]!='*')
+                    return 0;
+            }
+            return 1;
+        }
+        if(dp[i][j]!=-1) return dp[i][j];
+        int x=0,y=0;
+        if(s[i]==t[j]||t[j]=='?') return dp[i][j] = func(s,t,i-1,j-1,dp);
+        else if(t[j]=='*')
+        {
+            return dp[i][j]= func(s,t,i-1,j,dp)||func(s,t,i,j-1,dp);
+        }
+        else
+       return dp[i][j]=0;
+    }
+    bool isMatch(string s, string t) {
+        int n=s.length(),m=t.length();
+         vector<vector<int>>dp(n+1,vector<int>(m,-1));
+        return func(s,t,n-1,m-1,dp);
+    }
+};
