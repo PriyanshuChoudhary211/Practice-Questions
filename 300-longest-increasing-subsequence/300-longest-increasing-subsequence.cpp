3fdc65e515b858d12e30c122179a6236e6f2bc9f
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-07-24 11:37:47 +0530

    Time: 8 ms (94.83%), Space: 10.4 MB (87.63%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,3 +1,6 @@
+
+//MEMOIZATION BY DECLARING THE SIZE OF DP
+
 // class Solution {
 // public:
 //     int func(vector<int>&nums,int ind,int last,vector<vector<int>>&dp)
@@ -19,22 +22,54 @@
 //      return func(nums,0,-10001,dp);   
 //     }
 // };
+//
+
+//MEMOIZATION WITHOUT DECLARING THE SIZE
+
+// class Solution {
+// public:
+//     int find(vector<int>& nums,int ind,int last,vector<vector<int>> &dp){
+//         if(ind==nums.size())return 0;
+//         if(dp[ind][last+1]!=-1)return dp[ind][last+1];
+//         int pick=INT_MIN,n_pick=INT_MIN;
+        
+//         n_pick=find(nums,ind+1,last,dp);
+//         if(last==-1 || nums[ind]>nums[last]){
+//             pick=1+find(nums,ind+1,ind,dp);
+//         }
+//         return dp[ind][last+1]=max(pick,n_pick);
+//     }
+//     int lengthOfLIS(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<vector<int>>dp(n,vector<int>(n+1,-1));
+//         int x = find(nums,0,-1,dp);
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             for(int j=0;j<n+1;j++)
+//                 cout<<dp[i][j]<<" ";
+//             cout<<endl;
+//         }
+//         return x;
+//     }
+// };
+
+//USING BINARY SEARCH 
+
 class Solution {
 public:
-    int find(vector<int>& nums,int ind,int last,vector<vector<int>> &dp){
-        if(ind==nums.size())return 0;
-        if(dp[ind][last+1]!=-1)return dp[ind][last+1];
-        int pick=INT_MIN,n_pick=INT_MIN;
-        
-        n_pick=find(nums,ind+1,last,dp);
-        if(last==-1 || nums[ind]>nums[last]){
-            pick=1+find(nums,ind+1,ind,dp);
-        }
-        return dp[ind][last+1]=max(pick,n_pick);
-    }
-    int lengthOfLIS(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return find(nums,0,-1,dp);
-    }
+     int lengthOfLIS(vector<int>& nums) {
+         vector<int>ans;
+         ans.push_back(nums[0]);
+         for(int i=0;i<nums.size();i++)
+         {
+             if(nums[i]>ans.back())
+                 ans.push_back(nums[i]);
+             else
+             {
+                 int x=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                 ans[x]=nums[i];
+             }
+         }
+         return ans.size();
+     }
 };
