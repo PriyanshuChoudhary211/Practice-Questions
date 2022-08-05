064cb723d82418c2daaa1b640b394d6530dfeb31
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-08-05 22:17:15 +0530

    Time: 3 ms (36.03%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    int climb(int n,vector<int>&dp)
+    int func(int n,vector<int>&dp)
     {
         if(n==0) return 1;
-        else if(n<0) return 0;
         if(dp[n]!=-1) return dp[n];
-        int x = climb(n-1,dp);
-        int y = climb(n-2,dp);
-        return dp[n]=x+y;
+        int one=func(n-1,dp);
+        int two=0;
+        if(n-2>=0)
+           two=func(n-2,dp);
+        return dp[n]= one + two;
     }
     int climbStairs(int n) {
         vector<int>dp(n+1,-1);
-        return climb(n,dp);
+        return func(n,dp);
     }
 };
