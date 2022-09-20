66a3ff66dea52f4c7c5e73ae076497e11b8c8a4a
PriyanshuChoudhary211 <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2022-09-20 13:20:11 +0530

    Time: 35 ms (20.56%), Space: 14.3 MB (56.96%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,39 +10,13 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        map<TreeNode *, TreeNode *>mp;
-        queue<TreeNode*>que;
-        que.push(root);
-        while(!que.empty())
-        {
-            TreeNode *it =que.front();
-            que.pop();
-            if(it->left)
-            {
-                que.push(it->left);
-                mp[it->left]=it;
-             //   if(mp[it->left]==q)break;
-            }
-            if(it->right)
-            {
-                que.push(it->right);
-                mp[it->right]=it;
-                //if(mp[it->right]==q)break;
-            }
-        }
-        vector<int>ans;
-        ans.push_back(q->val);
-        while(mp.find(q)!=mp.end())
-        {
-            ans.push_back(mp[q]->val);
-            q=mp[q];
-        }
-        TreeNode *i=p;
-        while(find(ans.begin(),ans.end(),i->val)==ans.end())
-        {
-            i=mp[i];
-        }
-        return i;
-        
+        if(root==NULL) return NULL;
+        if(p==root || q==root) return root;
+        TreeNode *left=lowestCommonAncestor(root->left,p,q);
+        TreeNode *right=lowestCommonAncestor(root->right,p,q);
+        if(left==NULL) return right;
+        if(right==NULL) return left;
+        else 
+            return root;
     }
 };
