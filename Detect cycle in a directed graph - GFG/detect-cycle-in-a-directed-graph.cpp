d087e5d018c886cccc98e78cd92f2ceaf4da1a27
Priyanshu Choudhary <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2023-01-12 12:16:52 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,36 +6,44 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
+    void bfs(vector<int>adj[],queue<int>&q,vector<int>&ans,int indeg[])
+	{
+	    while(!q.empty())
+	    {
+	        int it=q.front();
+	        ans.push_back(it);
+	        q.pop();
+	        for(auto i:adj[it])
+	        {
+	            if(indeg[i]>0)
+	            {
+	                indeg[i]--;
+	            }
+	            if(indeg[i]==0)
+	            {
+	                q.push(i);
+	            }
+	        }
+	    }
+	}
     bool isCyclic(int V, vector<int> adj[]) {
-        vector<int>ans;
-        vector<int>indeg(V);
-        queue<int>q;
-        for(int i=0;i<V;i++)
-        {
-            for(auto it:adj[i])
-            {
-                indeg[it]++;
-            }
-        }
-        for(int i=0;i<V;i++)
-        {
-            if(indeg[i]==0)
-                q.push(i);
-        }
-        while(!q.empty())
-        {
-            int it=q.front();q.pop();
-            ans.push_back(it);
-            for(auto i:adj[it])
-            {
-                indeg[i]--;
-                if(indeg[i]==0)
-                    q.push(i);
-            }
-        }
-        if(ans.size()==V) return false;
-        return true;
-        
+         vector<int>ans;
+	   int indeg[V]={0};
+	   for(int i=0;i<V;i++)
+	   {
+	       for(auto it:adj[i])
+	            indeg[it]++;
+	   }
+	   queue<int>q;
+	   for(int i=0;i<V;i++)
+	   {
+	       if(indeg[i]==0)
+	        q.push(i);
+	   }
+	   bfs(adj,q,ans,indeg);  
+	    if(ans.size()==V) return false;
+	    return true;
+	
     }
 };
 
