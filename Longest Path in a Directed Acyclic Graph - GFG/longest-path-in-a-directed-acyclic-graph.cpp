adcc1dafc5fc9f02f8ef0325fe53c2daf8b8e84b
Priyanshu Choudhary <90238406+PriyanshuChoudhary211@users.noreply.github.com>
2023-01-30 18:37:38 +0530

    Added solution - LeetHub

---
+++ Longest Path in a Directed Acyclic Graph - GFG/longest-path-in-a-directed-acyclic-graph.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+      vector <int> maximumDistance(vector<vector<int>> edges,int v,int e,int src)
+      {
+            vector<int>dist(v,-1e9);
+            priority_queue<pair<int,int>>q;
+            vector<pair<int,int>>adj[v];
+            for(auto it:edges)
+            {
+                adj[it[0]].push_back({it[1],it[2]});
+            }
+            q.push({0,src});
+            dist[src]=0;
+            while(!q.empty())
+            {
+                auto it=q.top();
+                q.pop();
+                int dis=it.first;
+                int node=it.second;
+                for(auto i:adj[node])
+                {
+                    if(dist[i.first]<dis+i.second)
+                    {
+                        dist[i.first]=dis+i.second;
+                        q.push({dis+i.second,i.first});
+                    }
+                }
+            }
+            for(int i=0;i<v;i++)
+            {
+                if(dist[i]==-1e9)
+                {
+                    dist[i]=INT_MIN;
+                }
+            }
+            return dist;
+      }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+  
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int v,e;
+        cin>>v>>e;
+     
+        int src;
+        cin>>src;
+        vector<vector<int>> edges(e,vector<int> (3));
+        for(auto &j:edges)
+          cin>>j[0]>>j[1]>>j[2];
+
+        Solution s;
+        vector <int> ans=s.maximumDistance(edges,v,e,src);
+        for(auto j:ans)
+        {
+            if(j==INT_MIN)
+              cout<<"INF ";
+            else
+              cout<<j<<" ";
+        }
+        cout<<endl;
+    } 
+    return 0;
+ 
+}
+// } Driver Code Ends
